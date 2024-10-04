83233398a54997f37453f1c5310ad382278f9ccc
anshu9971 <arjunsingh0528159@gmail.com>
2024-10-04 12:42:19 +0530

    adding about section

---
+++ src/app/utils/constants/teams.js
@@ -0,0 +1,74 @@
+export const TeamsData = [
+    {
+        id:1,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/varun.png",
+        name:"Varun Chopra",
+        positon:"Co-Founder & Chief Executive Officer",
+        experience:"19+",
+        about:"Nomura, Deutsche IIT-Madras"
+    }, 
+    {
+        id:2,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/raheel.png",
+        name:"Raheel Shah",
+        positon:"Co-Founder",
+        experience:"16+",
+        about:"Accenture India Pvt. Ltd. IIM-Ahmedabad"  
+    },
+    {
+        id:3,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/parth.png",
+        name:"Parth Upadhyay",
+        positon:"Co-Founder",
+        experience:"10+",
+        about:"Riverhead Capital Founding team member Electrical Engineer"  
+    },
+    {
+        id:4,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Arvind.png",
+        name:"Arvind Sampath",
+        positon:"Chief Financial Officer",
+        experience:"29+",
+        about:"Poonawalla Fincorp, Fullerton Harvard Business School, FMS Delhi"  
+    },
+    {
+        id:5,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Harsh.png",
+        name:"Harshvardhan Singh Khangarot",
+        positon:"Chief Collections Officer",
+        experience:"19+",
+        about:"HSBC, Standard Chartered, Citi Financial SIMS, Pune"  
+    },
+    {
+        id:6,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Monojit.png",
+        name:"Monojit Banerjee",
+        positon:"Chief Technology Officer",
+        experience:"20+",
+        about:"Razorpay, Amazon, JP Morgan Chase & Co. IIT, Varanasi"  
+    },
+    {
+        id:7,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Simrata.png",
+        name:"Simrata Gujral",
+        positon:"Chief Legal & Compliance Officer",
+        experience:"16+",
+        about:"KKR, Deutsche, CITI,J.P.Morgan Delhi University"  
+    },
+    {
+        id:8,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Loveleen.png",
+        name:"Loveleen Sahrawat",
+        positon:"Chief Marketing Officer",
+        experience:"29+",
+        about:"Aditya Birla Capital, McCann Worldgroup IIMS"  
+    },
+    {
+        id:9,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Hanish-Dewan.png",
+        name:"Hanish Dewan",
+        positon:"Chief Human Resources Officer",
+        experience:"17+",
+        about:"Times Network, KPMG, Lenskart, Wipro IMI, Belgium"  
+    },
+]
