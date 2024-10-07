6f49bd6a01fcc138e95c19307efebcc7db23f053
anshu9971 <arjunsingh0528159@gmail.com>
2024-10-07 15:15:44 +0530

    adding impact and about and career page

---
+++ src/app/(about)/impact/page.jsx
@@ -0,0 +1,114 @@
+"use client";
+
+import Image from "next/image";
+import { impactStats, userReviewData } from "utils/constants/about";
+import styles from "./impact.module.scss";
+
+export default function Impact() {
+    return (
+        <div className={styles.container}>
+            <section className={styles.section1}>
+                <div className={styles.div2}>
+                    <h2>OUR</h2>
+                    <h1>Impact</h1>
+                    <p>
+                        We re democratising access to quality education. Over
+                        the last few years, we have touched the lives of
+                        thousands of students from hundreds of institutes across
+                        India by helping them get low-cost & flexible education
+                        finance.
+                    </p>
+                </div>
+                <div>
+                    <Image
+                        src="https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/our-impact-hero-img.png"
+                        alt="about section image"
+                        width={500}
+                        height={600}
+                        layout="responsive"
+                    />
+                </div>
+            </section>
+            <section className={styles.section2}>
+                <div className={styles.statWrapper}>
+                    {impactStats &&
+                        impactStats.map((stat) => (
+                            <div key={stat.id}>
+                                <h4>{stat.heading}</h4>
+                                <div>
+                                    <Image
+                                        src={stat.icon}
+                                        alt={stat.heading}
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <div>{stat.count}</div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </section>
+            <div className={styles.section3}>
+                <h1>What our users have to say...</h1>
+                <div>
+                    {userReviewData.map((user) => (
+                        <div key={user.id} className={styles.wrapper}>
+                            <div className={styles.name}>
+                                <Image
+                                    src={user.image}
+                                    alt={user.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <div>
+                                    <h5>{user.name}</h5>
+                                    <p>{user.company}</p>
+                                </div>
+                            </div>
+                            <div className={styles.education}>
+                                <div className={styles.course}>
+                                    <h6>Institude</h6>
+                                    <p>{user.institude}</p>
+                                </div>
+                                <div className={styles.course}>
+                                    <h6>Course</h6>
+                                    <p>{user.course}</p>
+                                </div>
+                            </div>
+                            <p>
+                                {user.para} 
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.section4}>
+                <h1>CSR</h1>
+                <div>
+                    <Image
+                        src="https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/csr-img.png"
+                        alt="about section image"
+                        width={300}
+                        height={300}
+                        // layout="responsive"
+                        className={styles.csrImage}
+                    />
+                    <p>
+                        Even though Simarjeet Kaur scored 449 out of 500, she
+                        was unsure of continuing her studies due to the poor
+                        financial condition of her family. My dream is to
+                        become an IAS officer one day, said Simarjeet Kaur who
+                        hails from Budhlada, a small town in Punjab. We came
+                        across Simarjeet&quot;s story via an Indian Express Article.
+                        When we spoke to her, Simarjit was proud of her
+                        achievement, but also constrained that she would not be
+                        able to pursue her dream. Eduvanz decided to help the
+                        hardworking & ambitious girl at all costs the moment the
+                        story was published, in line with our motto of Fuelling
+                        the career growth of young India!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/utils/constants/about.js
@@ -0,0 +1,171 @@
+export const TeamsData = [
+    {
+        id:1,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/varun.png",
+        name:"Varun Chopra",
+        positon:"Co-Founder & Chief Executive Officer",
+        experience:"19+",
+        about:"Nomura, Deutsche IIT-Madras"
+    }, 
+    {
+        id:2,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/raheel.png",
+        name:"Raheel Shah",
+        positon:"Co-Founder",
+        experience:"16+",
+        about:"Accenture India Pvt. Ltd. IIM-Ahmedabad"  
+    },
+    {
+        id:3,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/parth.png",
+        name:"Parth Upadhyay",
+        positon:"Co-Founder",
+        experience:"10+",
+        about:"Riverhead Capital Founding team member Electrical Engineer"  
+    },
+    {
+        id:4,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Arvind.png",
+        name:"Arvind Sampath",
+        positon:"Chief Financial Officer",
+        experience:"29+",
+        about:"Poonawalla Fincorp, Fullerton Harvard Business School, FMS Delhi"  
+    },
+    {
+        id:5,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Harsh.png",
+        name:"Harshvardhan Singh Khangarot",
+        positon:"Chief Collections Officer",
+        experience:"19+",
+        about:"HSBC, Standard Chartered, Citi Financial SIMS, Pune"  
+    },
+    {
+        id:6,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Monojit.png",
+        name:"Monojit Banerjee",
+        positon:"Chief Technology Officer",
+        experience:"20+",
+        about:"Razorpay, Amazon, JP Morgan Chase & Co. IIT, Varanasi"  
+    },
+    {
+        id:7,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Simrata.png",
+        name:"Simrata Gujral",
+        positon:"Chief Legal & Compliance Officer",
+        experience:"16+",
+        about:"KKR, Deutsche, CITI,J.P.Morgan Delhi University"  
+    },
+    {
+        id:8,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Loveleen.png",
+        name:"Loveleen Sahrawat",
+        positon:"Chief Marketing Officer",
+        experience:"29+",
+        about:"Aditya Birla Capital, McCann Worldgroup IIMS"  
+    },
+    {
+        id:9,
+        img:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/about/Hanish-Dewan.png",
+        name:"Hanish Dewan",
+        positon:"Chief Human Resources Officer",
+        experience:"17+",
+        about:"Times Network, KPMG, Lenskart, Wipro IMI, Belgium"  
+    },
+]
+
+
+export const impactStats = [
+ {
+    id:1,
+    heading:"Institute Partnerships",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-school.svg",
+    count:"900+"
+ },
+ {
+    id:2,
+    heading:"Students Financed By Us",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-graduation-cap.svg",
+    count:"200,000+"
+ },
+ {
+    id:3,
+    heading:"Loans Disbursed",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-graduation-cap.svg",
+    count:"3000+Cr"
+ },
+ {
+    id:4,
+    heading:"Courses",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-graduation-cap.svg",
+    count:"7500+"
+ },
+ {
+    id:5,
+    heading:"Cities Covered",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-graduation-cap.svg",
+    count:"240"
+ },
+ {
+    id:6,
+    heading:"Lending Partners",
+    icon:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/icon-graduation-cap.svg",
+    count:"23"
+ },
+]
+
+export const userReviewData = [
+    {
+        id:1,
+        image:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/sreehari.png",
+        name:"Sreehari R",
+        company:"Infosys",
+        institude:"Great Learning",
+        course:"PGP Data Science",
+        para:"I appreciate the transparency in the overall process. High-speed loan disbursal with great service. I liked the No cost interest which is very profitable for students."
+    },
+    {
+        id:2,
+        image:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/prasadpolisetty.png",
+        name:"Prasad Polisetty",
+        company:"Wipro",
+        institude:"eDrishyaa",
+        course:"SAP Course",
+        para:"The entire team was very responsive and approachable. Quick responses to all my queries. My documents were immediately processed and the loan was issued very quickly."
+    },
+    {
+        id:3,
+        image:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/amitpande.png",
+        name:"Amit Pande",
+        company:"Axis Securities",
+        institude:"Times Pro",
+        course:"PCDM",
+        para:"Very grateful to Eduvanz for helping me out by financing this course. Everything was very smooth and hassle-free. The course was great, even beyond my expectation. I was looking for a new job..."
+    },
+    {
+        id:4,
+        image:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/sivasankaran.png",
+        name:"Sivasankaran Swaminarayana",
+        company:"TCS",
+        institude:"Great Learning",
+        course:"PGP-AIML-Online",
+        para:"Eduvanz has been of great help to get the loan and proceed with my dream course quickly. I always got immediate help and support from Eduvanz. Their help is so good that I never needed..."
+    },
+    {
+        id:5,
+        image:"	https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/vkumar.png",
+        name:"V Kumar",
+        company:"Hitachi",
+        institude:"Imarticus Learning",
+        course:"Data Science Prodegree",
+        para:"The smooth and completely online application process, no manual intervention was required ..."
+    },
+    {
+        id:6,
+        image:"https://d1idiaqkpcnv43.cloudfront.net/assets/webimages/RuchikaJain.jpg",
+        name:"Ruchika Jain",
+        company:"Ericson",
+        institude:"Great Learning",
+        course:"PGP - Artificial Intelligence for Leaders",
+        para:"Process of getting the loan was very smooth and was very well guided in..."
+    },
+]
